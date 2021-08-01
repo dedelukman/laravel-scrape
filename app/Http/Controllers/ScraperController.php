57a2653ae4aa9e2f661501190203d7978eced0bd
dedelukman <dlukmanul.h@gmail.com>
2021-08-01 21:53:35 +0700

    cek get data scraper with clinet goutte

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Goutte\Client;
 use Illuminate\Http\Request;
+use Nette\Utils\Arrays;
 
 class ScraperController extends Controller
 {
+    private $result = array();
+
     public function Scraper (){
+        
+        $client = new Client();
+        
+        $url ='https://www.worldometers.info/coronavirus/';
+
+        $page = $client->request('GET', $url);
+
+        echo "<pre>";
+        print_r($page);
+
+
         return view('scraper');
     }
 }
